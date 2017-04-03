01ba7dd3e8c6c846d74ae9007c57d13753f2e86b
Applied CompSci (Educational Practical CompSci Examples) <compscied@users.noreply.github.com>
2017-04-02 20:59:45 -0400

    Update tensorflow-ubuntu-install.sh

--- tensorflow-ubuntu-install.sh
+++ tensorflow-ubuntu-install.sh
@@ -1,4 +1,6 @@
 
 #ref https://www.tensorflow.org/install/install_linux#InstallingNativePip
 sudo apt-get install python-pip python-dev
-pip install tensorflow  
+pip install tensorflow-gpu 
+#cpu version
+#pip install tensorflow
