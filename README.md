9ff1577cb27673694cd4d0515f1ff3fd1761c4e9
Jonas Malmsten <jonas@malmsten.net>
2017-04-27 00:25:35 -0400

    Removed ".gz" for dataset filenames Removed most comments to fit in paper

--- README.md
+++ README.md
@@ -6,10 +6,13 @@ https://www.tensorflow.org/install/
 # Distributed Installation
 https://www.tensorflow.org/deploy/distributed
 
-# Dataset to be used can be downloaded from here
+# Dataset is available in the RAR archives, just unpack with folders.
+# If you don't have RAR, it can also be downloaded from here:
 https://drive.google.com/drive/folders/0B_gHChulW1atSkppYW9mLUo3Snc?usp=sharing
 
 # Original dataset
 https://drive.google.com/file/d/0B_gHChulW1atdmI4X24xUkwwa2s/view?usp=sharing
+
+# To train the network, run EmbryoConvolutionLearning.py
 
 
--- mnistembryo.py
+++ mnistembryo.py
@@ -1,59 +1,24 @@
+# mnistembryo.py
 # Copyright 2016 The TensorFlow Authors. All Rights Reserved.
-#
 # Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 # Modified for Embryo Data by DPS2018 Team 2
-# ==============================================================================
-
-"""Functions for downloading and reading Embryo data."""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
-import gzip
-
 import numpy
 from six.moves import xrange  # pylint: disable=redefined-builtin
-
 from tensorflow.contrib.learn.python.learn.datasets import base
 from tensorflow.python.framework import dtypes
-
 def _read32(bytestream):
   dt = numpy.dtype(numpy.uint32).newbyteorder('>')
   return numpy.frombuffer(bytestream.read(4), dtype=dt)[0]
-
-
 def extract_images(f):
-  """Extract the images into a 4D uint8 numpy array [index, y, x, depth].
-
-  Args:
-    f: A file object that can be passed into a gzip reader.
-
-  Returns:
-    data: A 4D uint8 numpy array [index, y, x, depth].
-
-  Raises:
-    ValueError: If the bytestream does not start with 2051.
-
-  """
   print('Extracting', f.name)
   #with gzip.GzipFile(fileobj=f) as bytestream:
   with f as bytestream:
     magic = _read32(bytestream)
     if magic != 2051:
-      raise ValueError('Invalid magic number %d in EMBRYO image file: %s' %
-                       (magic, f.name))
+      raise ValueError('Invalid magic number %d in EMBRYO image file: %s' % (magic, f.name))
     num_images = _read32(bytestream)
     rows = _read32(bytestream)
     cols = _read32(bytestream)
@@ -61,113 +26,55 @@ def extract_images(f):
     data = numpy.frombuffer(buf, dtype=numpy.uint8)
     data = data.reshape(num_images, rows, cols, 1)
     return data
-
-
 def dense_to_one_hot(labels_dense, num_classes):
-  """Convert class labels from scalars to one-hot vectors."""
   num_labels = labels_dense.shape[0]
   index_offset = numpy.arange(num_labels) * num_classes
   labels_one_hot = numpy.zeros((num_labels, num_classes))
   labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
   return labels_one_hot
-
-
 def extract_labels(f, one_hot=False, num_classes=4):
-  """Extract the labels into a 1D uint8 numpy array [index].
-
-  Args:
-    f: A file object that can be passed into a gzip reader.
-    one_hot: Does one hot encoding for the result.
-    num_classes: Number of classes for the one hot encoding.
-
-  Returns:
-    labels: a 1D uint8 numpy array.
-
-  Raises:
-    ValueError: If the bystream doesn't start with 2049.
-  """
   print('Extracting', f.name)
   with f as bytestream:
     magic = _read32(bytestream)
     if magic != 2049:
-      raise ValueError('Invalid magic number %d in EMBRYO label file: %s' %
-                       (magic, f.name))
+      raise ValueError('Invalid magic number %d in EMBRYO label file: %s' % (magic, f.name))
     num_items = _read32(bytestream)
     buf = bytestream.read(num_items)
     labels = numpy.frombuffer(buf, dtype=numpy.uint8)
     if one_hot:
       return dense_to_one_hot(labels, num_classes)
     return labels
-
-
 class DataSet(object):
-
-  def __init__(self,
-               images,
-               labels,
-               fake_data=False,
-               one_hot=False,
-               dtype=dtypes.float32,
-               reshape=True):
-    """Construct a DataSet.
-    one_hot arg is used only if fake_data is true.  `dtype` can be either
-    `uint8` to leave the input as `[0, 255]`, or `float32` to rescale into
-    `[0, 1]`.
-    """
+  def __init__(self,images,labels,one_hot=False,dtype=dtypes.float32,reshape=True):
     dtype = dtypes.as_dtype(dtype).base_dtype
     if dtype not in (dtypes.uint8, dtypes.float32):
-      raise TypeError('Invalid image dtype %r, expected uint8 or float32' %
-                      dtype)
-    if fake_data:
-      self._num_examples = 10000
-      self.one_hot = one_hot
-    else:
-      assert images.shape[0] == labels.shape[0], (
-          'images.shape: %s labels.shape: %s' % (images.shape, labels.shape))
-      self._num_examples = images.shape[0]
-
-      # Convert shape from [num examples, rows, columns, depth]
-      # to [num examples, rows*columns] (assuming depth == 1)
-      if reshape:
-        assert images.shape[3] == 1
-        images = images.reshape(images.shape[0],
-                                images.shape[1] * images.shape[2])
-      if dtype == dtypes.float32:
-        # Convert from [0, 255] -> [0.0, 1.0].
-        images = images.astype(numpy.float32)
-        images = numpy.multiply(images, 1.0 / 255.0)
+      raise TypeError('Invalid image dtype %r, expected uint8 or float32' % dtype)
+    assert images.shape[0] == labels.shape[0], (
+        'images.shape: %s labels.shape: %s' % (images.shape, labels.shape))
+    self._num_examples = images.shape[0]
+    if reshape:
+      assert images.shape[3] == 1
+      images = images.reshape(images.shape[0], images.shape[1] * images.shape[2])
+    if dtype == dtypes.float32:
+      images = images.astype(numpy.float32)
+      images = numpy.multiply(images, 1.0 / 255.0)
     self._images = images
     self._labels = labels
     self._epochs_completed = 0
     self._index_in_epoch = self._num_examples + 1 # force shuffle in first batch
-
   @property
   def images(self):
     return self._images
-
   @property
   def labels(self):
     return self._labels
-
   @property
   def num_examples(self):
     return self._num_examples
-
   @property
   def epochs_completed(self):
     return self._epochs_completed
-
-  def next_batch(self, batch_size, fake_data=False):
-    """Return the next `batch_size` examples from this data set."""
-    if fake_data:
-      fake_image = [1] * 784
-      if self.one_hot:
-        fake_label = [1] + [0] * 9
-      else:
-        fake_label = 0
-      return [fake_image for _ in xrange(batch_size)], [
-          fake_label for _ in xrange(batch_size)
-      ]
+  def next_batch(self, batch_size):
     start = self._index_in_epoch
     self._index_in_epoch += batch_size
     if self._index_in_epoch > self._num_examples:
@@ -184,58 +91,27 @@ class DataSet(object):
       assert batch_size <= self._num_examples
     end = self._index_in_epoch
     return self._images[start:end], self._labels[start:end]
-
-
-def read_data_sets(train_dir,
-                   fake_data=False,
-                   one_hot=False,
-                   dtype=dtypes.float32,
-                   reshape=True,
-                   validation_size=500):
-  if fake_data:
-
-    def fake():
-      return DataSet([], [], fake_data=True, one_hot=one_hot, dtype=dtype)
-
-    train = fake()
-    validation = fake()
-    test = fake()
-    return base.Datasets(train=train, validation=validation, test=test)
-
-  TRAIN_IMAGES = '64/Train/Data.dat.gz'
-  TRAIN_LABELS = '64/Train/Idx.dat.gz'
-  TEST_IMAGES = '64/Test/Data.dat.gz'
-  TEST_LABELS = '64/Test/Idx.dat.gz'
-
+def read_data_sets(train_dir,one_hot=False,dtype=dtypes.float32,reshape=True,validation_size=500):
+  TRAIN_IMAGES = '64/Train/Data.dat'
+  TRAIN_LABELS = '64/Train/Idx.dat'
+  TEST_IMAGES = '64/Test/Data.dat'
+  TEST_LABELS = '64/Test/Idx.dat'
   with open(TRAIN_IMAGES, 'rb') as f:
     train_images = extract_images(f)
-
   with open(TRAIN_LABELS, 'rb') as f:
     train_labels = extract_labels(f, one_hot=one_hot)
-
   with open(TEST_IMAGES, 'rb') as f:
     test_images = extract_images(f)
-
   with open(TEST_LABELS, 'rb') as f:
     test_labels = extract_labels(f, one_hot=one_hot)
-
   if not 0 <= validation_size <= len(train_images):
-    raise ValueError(
-        'Validation size should be between 0 and {}. Received: {}.'
+    raise ValueError('Validation size should be between 0 and {}. Received: {}.'
         .format(len(train_images), validation_size))
-
   validation_images = train_images[:validation_size]
   validation_labels = train_labels[:validation_size]
   train_images = train_images[validation_size:]
   train_labels = train_labels[validation_size:]
-
   train = DataSet(train_images, train_labels, dtype=dtype, reshape=reshape)
-  validation = DataSet(validation_images,
-                       validation_labels,
-                       dtype=dtype,
-                       reshape=reshape)
+  validation = DataSet(validation_images,validation_labels,dtype=dtype,reshape=reshape)
   test = DataSet(test_images, test_labels, dtype=dtype, reshape=reshape)
-
   return base.Datasets(train=train, validation=validation, test=test)
-
-
